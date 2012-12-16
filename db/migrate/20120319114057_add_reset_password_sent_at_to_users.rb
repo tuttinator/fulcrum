b37debefb3eb1df140b176930d7a089e55d1f08d
Caleb <caleb@prettymint.co.nz>
2012-12-16 21:22:47 +1300

    Comment about broken migration

--- db/migrate/20120319114057_add_reset_password_sent_at_to_users.rb
+++ db/migrate/20120319114057_add_reset_password_sent_at_to_users.rb
@@ -1,6 +1,8 @@
+# This migration produces errors with PG 9.1 - already run
+
 class AddResetPasswordSentAtToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :reset_password_sent_at, :datetime
+    # add_column :users, :reset_password_sent_at, :datetime
 
   end
 end
